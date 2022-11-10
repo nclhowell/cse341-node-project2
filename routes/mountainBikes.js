b026375fd827d4a25bf17b577e16ddf60c9c0c6f
nclhowell <nclhowell@gmail.com>
2022-11-09 17:45:48 -0800

    Lesson 7 OAuth working using auth0 service!

--- routes/mountainBikes.js
+++ routes/mountainBikes.js
@@ -2,15 +2,41 @@ const express = require('express');
 const router = express.Router();
 const mountainBikesController = require('../controllers/mountainBikes');
 const validation = require('../middleware/validate');
+const app = express();
+require("dotenv").config();
+const {auth, requiresAuth} = require("express-openid-connect")
 
-router.get('/', mountainBikesController.getAll);
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET, 
+    baseURL: process.env.BASE_URL, 
+    clientID:  process.env.CLIENT_ID, 
+    issuerBaseURL: process.env.ISSUER_BASE_URL
+  };
+  
+  // auth router attaches /login, /logout, and /callback routes to the baseURL
+  // app.use(auth(config));
+  router.use(auth(config));
+  
+  // req.isAuthenticated is provided from the auth router
+  // router.get('/', (req, res) => {
+  //  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  //});
 
-router.get('/:id', mountainBikesController.getSingle);
+  // app.get('/profile', requiresAuth(), (req, res) => {
+  //  res.send(JSON.stringify(req.oidc.user)); 
+  //  console.log(JSON.stringify(req.oidc.user.given_name)); 
+  // });
 
-router.post('/', validation.saveMtb, mountainBikesController.createSingle);
+router.get('/', requiresAuth(), mountainBikesController.getAll);
 
-router.put('/:id', validation.saveMtb, mountainBikesController.updateSingle);
+router.get('/:id', requiresAuth(), mountainBikesController.getSingle);
 
-router.delete('/:id', mountainBikesController.deleteSingle);
+router.post('/', requiresAuth(), validation.saveMtb, mountainBikesController.createSingle);
+
+router.put('/:id', requiresAuth(), validation.saveMtb, mountainBikesController.updateSingle);
+
+router.delete('/:id', requiresAuth(), mountainBikesController.deleteSingle);
 
 module.exports = router;
