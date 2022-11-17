1bb372a20df49739855511d9ae6038a1d26eb526
Jashua <77981430+81Jashua@users.noreply.github.com>
2022-11-16 20:16:26 -0700

    adding basic function for functions class

--- helpers/functions.js
+++ helpers/functions.js
@@ -1,1 +1,15 @@
-//This is the new functions file
+//Purpose: return a list of objects [{bike}, {helmet}, {shoes}]
+const fooController = require('../controllers/foo');
+
+function userRecommendations(bikePref, helmetPref, shoesPref) {
+    let userItems = [];
+    let bike = fooController.getBike(bikePref);
+    let helmet = fooController.getHelmet(helmetPref);
+    let shoes = fooController.getShoes(shoesPref);
+
+    userItems.push(bike);
+    userItems.push(helmet);
+    userItems.push(shoes);
+
+    return userItems;
+}
