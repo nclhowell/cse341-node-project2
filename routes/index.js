187a6bdd9fd0ac44814208fd4ef3ded83229532c
Samuel Palacios <pal20010@byui.edu>
2022-11-16 20:27:17 -0600

    This is a test of commiting

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/mountainBikes', require('./mountainBikes'))
-// router.use('/users', require('./users'))
+router.use('/users', require('./users'))
+router.use('/helmets', require('./helmets'))
+router.use('/shoes', require('./shoes'))
 router.use('/swagdocs', require('./swag'))
 
 module.exports = router;
