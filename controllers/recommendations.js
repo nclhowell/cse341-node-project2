d2086cdae009322423d76ae33af502426d3530b6
Jashua <77981430+81Jashua@users.noreply.github.com>
2022-11-28 18:47:11 -0700

    playing with functions

--- controllers/recommendations.js
+++ controllers/recommendations.js
@@ -2,20 +2,23 @@ const mongodb = require("../db/connect");
 
 //Purpose is to provide the helpers/functions.js with a bike collection, helmet collection and shoe collection
 
-const getBike = async (req, res, next, bikeInfo) => {
-    const result = await mongodb
-      .getDb()
-      .db("project2")
-      .collection("mountainBikes")
-      .find({"terrainLevel": bikeInfo,});
-    result.toArray((err, result) => {
-      if (err) {
-        res.status(400).json({ message: err });
-      }
-      res.setHeader("Content-Type", "application/json");
-      res.status(200).json(result[0]);
-    });
-  };
+function getBike(bikeInfo) {
+    const bikeCollection = async (req, res, next) => {
+        const result = await mongodb
+          .getDb()
+          .db("project2")
+          .collection("mountainBikes")
+          .find({"terrainLevel": bikeInfo,});
+        result.toArray((err, result) => {
+          if (err) {
+            res.status(400).json({ message: err });
+          }
+          res.setHeader("Content-Type", "application/json");
+          res.status(200).json(result[0]);
+        });
+      };
+    return bikeCollection;
+}
 
   const getHelmet = async (req, res, next, helmetInfo) => {
     const result = await mongodb
