5d37b9e4206e7a39c576b6716142a92a197b4098
Samuel Palacios <pal20010@byui.edu>
2022-11-30 22:58:04 -0600

    Fixing shoes router, it was missing config for authenticaton function

--- routes/shoes.js
+++ routes/shoes.js
@@ -4,6 +4,17 @@ const shoesController = require('../controllers/shoes');
 const validation = require('../middleware/validate');
 const {auth, requiresAuth} = require("express-openid-connect");
 
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET, 
+    baseURL: process.env.BASE_URL, 
+    clientID:  process.env.CLIENT_ID, 
+    issuerBaseURL: process.env.ISSUER_BASE_URL
+  };  
+
+router.use(auth(config));
+
 router.get('/', requiresAuth(), shoesController.getAll);
 
 router.get('/:id', requiresAuth(), shoesController.getSingle);
